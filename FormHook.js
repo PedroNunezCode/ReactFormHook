24824028d136e954bf71c7168ea21a02b86e1af9
PedroNunezCode <pedronunezcode@gmail.com>
2022-07-01 12:22:58 -0700

    TABS OR SPACES?

--- FormHook.js
+++ FormHook.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const useForm = (initialState = {}) => {
-		const [formState, setFormState] = React.useState(initialState);
-		const handleInputChange = (e) => {
-					setFormState({ ...formState, [e.target.name]: e.target.value });
-				}
-		return { formState, handleInputChange }
+    const [formState, setFormState] = React.useState(initialState);
+    const handleInputChange = (e) => {
+	setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    return { formState, handleInputChange }
 }
